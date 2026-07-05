c1dadd5f60cfd411e72991dc04d83ee04429e701
Ben WU <benwu@Bens-MacBook-Air.local>
2026-07-05 10:59:38 +0800

    daily backup 2026-07-05

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  site: 'https://anu.wiki',
+  vite: { plugins: [tailwindcss()] }, integrations: [sitemap()],
 });
